4b780edcddcc34a7ad66f1bf8a2d0ef49cbb489b
Easy Programming <easyprogrammingdev@gmail.com>
2024-03-08 20:02:06 +0000

    exemplos

---
+++ Lógica de Programação/Modulos/exemplo-sqrt.py
@@ -0,0 +1,12 @@
+import math
+
+
+numero = int(input("Digite um numero para calcular sua raiz quadrada: "))
+
+raiz = math.sqrt(numero)
+
+
+
+print("A raiz de quadrada {} é {:.2f}.".format(numero,raiz))
+
+
