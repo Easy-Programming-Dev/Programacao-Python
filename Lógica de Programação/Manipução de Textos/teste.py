1e58f9550c8dd24de82ef9e2a434605ee53d022a
Easy-Programming-Dev <160754667+Easy-Programming-Dev@users.noreply.github.com>
2024-03-12 20:57:02 +0000

    update

---
+++ Lógica de Programação/Manipução de Textos/teste.py
@@ -0,0 +1,23 @@
+############################# FATIAMENTO #############################
+curso = 'logica de programação'
+
+#print(curso[10])
+
+#print(curso[10:17])
+
+#print(curso[10:21:2])
+
+#print(curso[:5])
+
+#print(curso[7:])
+
+#print(curso[10::3])
+
+
+
+############################# ANALISE #############################
+
+
+
+
+
