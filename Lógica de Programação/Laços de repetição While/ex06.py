8e8aba3bd97a390c62b2427699b9aa039da69aa1
Easy-Programming-Dev <160754667+Easy-Programming-Dev@users.noreply.github.com>
2024-03-23 00:12:30 +0000

    update

---
+++ Lógica de Programação/Laços de repetição While/ex06.py
@@ -0,0 +1,14 @@
+'''6. Faça um programa que receba vários números inteiros repetidamente. 
+Se o número digitado não for primo, o programa deverá mostrar a mensagem "não é primo". 
+Caso contrario o programa deve encerrar, mostrando no final do código qual o número primo digitado. 
+'''
+
+while True:
+    numero = int(input("\nDigite um número: "))
+
+    if numero % 2 == 0 and numero != 2:
+        print("não primo")
+    else:
+        break
+
+print(f"O número {numero} é primo")
---
+++ Lógica de Programação/Laços de repetição While/ex08.py
@@ -0,0 +1,11 @@
+''' Faça um programa em Python utilizando a estrutura de repetição WHILE, 
+que leia um valor inteiro e mostre a tabuada de 1 a 10 do valor lido. Exemplo: “1 x 2 = 2”.'''
+
+
+n = int(input("digite um numero de 1 a 10: "))
+cont = 1
+
+while cont <= 10:
+    tab = n * cont
+    print(n, "X", cont, "=", tab)
+    cont = cont + 1
